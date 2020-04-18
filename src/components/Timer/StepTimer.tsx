5ce701c4af015bd1cf926c56827de95cbc5be2f6
Jonathan Tuzman <tuzmusic@gmail.com>
2020-04-18 11:11:35 -0400

    rewrite StepTimerContainer using ownProps

--- src/components/Timer/StepTimer.tsx
+++ src/components/Timer/StepTimer.tsx
@@ -7,14 +7,10 @@ import { AppDispatch } from "../../redux/store";
 import { setTimerState } from "../../redux/cookingSessionSlice";
 import { connect } from "react-redux";
 
-type Props = {
-  timer: StepTimer
-  startTimer: Function
-};
-
 const TimerWrapper = styled.div({
   textAlign: 'center',
 })
+
 const StepTimerButton = styled.button({
   borderRadius: 50,
   height: '60px',
@@ -22,16 +18,19 @@ const StepTimerButton = styled.button({
   display: 'block',
   fontSize: 18
 })
-
 const DurationWrapper = styled(CenterFlexRow)({
   width: '100%',
 })
+
+type Props = {
+  timer: StepTimer
+  startTimer?: any // FIXME
+};
 const StepTimerComponent = ({ timer, startTimer }: Props) =>
   <TimerWrapper>
     { timer.label }
-    <StepTimerButton onClick={ () => startTimer(timer) }>
-      <Timer direction="backward"
-             initialTime={ timer.durationSec * 1000 }
+    <StepTimerButton onClick={ startTimer }>
+      <Timer direction="backward" initialTime={ timer.durationSec * 1000 }
              startImmediately={ timer.state === CookingTimerState.Running }
       >{/* Any actual whitespace between the closing carat and the opening brace will BREAK the timer display */ }
         { ({ start, resume, pause, stop, reset, timerState }: any) =>
@@ -46,10 +45,8 @@ const StepTimerComponent = ({ timer, startTimer }: Props) =>
     </StepTimerButton>
   </TimerWrapper>
 
-const actions = (dispatch: AppDispatch) => ({
-  startTimer: (timer: StepTimer) => dispatch(
-    setTimerState({ timer, timerState: CookingTimerState.Running })
-  )
+const actions = (dispatch: AppDispatch, { timer }: Props) => ({
+  startTimer: () => dispatch(setTimerState({ timer, timerState: CookingTimerState.Running }))
 })
 
 const StepTimerContainer = connect(null, actions)(StepTimerComponent)
