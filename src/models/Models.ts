453bd36db46b2601bcbc4f9510a83395b73a320e
Jonathan Tuzman <tuzmusic@gmail.com>
2020-04-25 15:55:13 -0400

    write new POJO recipe parser

--- src/models/Models.ts
+++ src/models/Models.ts
@@ -1,5 +1,5 @@
 // exists immutably in the database, but is *copied* by value for every session
-import { IngredientJSON, IngredientState, RecipeInfo, RecipeJSON } from "../redux/state/stateMap";
+import { AppRecipe, IngredientJSON, IngredientState, RecipeInfo, RecipeJSON } from "../redux/state/stateMap";
 
 export class Recipe {
   info: RecipeInfo = {
@@ -9,6 +9,43 @@ export class Recipe {
   instructions: Instruction[] = [];
   ingredients: Ingredient[] = [];
   timers: StepTimer[] = [];
+  
+  static parseJsonToJson = (json: RecipeJSON): AppRecipe => {
+    let timerIndex = 0;
+    let ingredientIndex = 0;
+    
+    const recipe: AppRecipe = {
+      info: { id: "", title: json.title },
+      instructions: [], ingredients: [], timers: []
+    }
+    
+    json.instructions.forEach((inst, stepIndex) => {
+      const instruction = new Instruction(inst.text)
+      
+      inst.timers.forEach(({ label, durationSec }) => {
+        recipe.timers.push({
+          label, durationSec, stepIndex,
+          id: timerIndex++,
+          state: CookingTimerState.Pending
+        });
+      })
+      
+      // todo: this orders ingredients by step, which could lead to duplicate
+      //  ingredients (and a differing order between printed recipe and parsed recipe.
+      //  at the simplest, we should keep a list of all ingredients
+      //  separate from their relationship to instructions.
+      inst.ingredients.forEach(({ text }: IngredientJSON) => {
+        recipe.ingredients.push({
+          text, stepIndex, state: { done: false },
+          id: ingredientIndex++
+        })
+      })
+      
+      recipe.instructions.push(instruction)
+    })
+    
+    return recipe
+  }
   
   static parseJson = (json: RecipeJSON): Recipe => {
     const recipe = new Recipe()
